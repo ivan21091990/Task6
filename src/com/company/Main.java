c4083a66f08c8120c0135ea6f0ea9162a748179c
AndreevIP <Andreev2102@inbox.ru>
2019-08-15 18:02:46 +0300

    Задача 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число x и нажмите Enter:");
+        int x = scanner.nextInt();
+        System.out.println("Введите число y и нажмите Enter:");
+        int y = scanner.nextInt();
+        System.out.println("Введите число z и нажмите Enter:");
+        int z = scanner.nextInt();
+        int a = (x + y + z) / 3;
+        System.out.println("Среднее арифметическое x,y,z = " + a);
+        int b = a / 2;
+        System.out.println("Деление среднего арифметического на 2 = " + b);
+              if (b > 3)
+        {
+            System.out.println("Программа выполнена корректно(" + b + " > 3)");
+        }
     }
 }
